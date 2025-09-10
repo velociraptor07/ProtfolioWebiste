9c3ada298212a3707eeab96fe07e806119c5137f
velociraptor07 <sachdevyash61@gmail.com>
2025-09-10 16:32:10 -0400

    State file checks

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,17 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "yash-tfstate-rg"
-    storage_account_name  = "yashtfstateczukxo"   
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
+    # Remote state (pre-existing, do not manage with TF)
+    resource_group_name  = "yash-tfstate-rg"
+    storage_account_name = "yashtfstate565896"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -11,33 +19,37 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "yash-tfstate-rg"
+# -------- Managed APP infrastructure --------
+resource "azurerm_resource_group" "app_rg" {
+  name     = "yash-portfolio-rg"
   location = "Australia Central"
 }
 
-resource "azurerm_storage_account" "sa" {
-  name                     = "yashtfstate${random_string.suffix.result}" 
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_service_plan" "plan" {
+  name                = "yash-portfolio-plan"
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+  os_type             = "Windows"
+  sku_name            = "S1"
 }
 
-resource "azurerm_storage_container" "container" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = "private"
+resource "azurerm_windows_web_app" "app" {
+  name                          = "yash-portfolio-new"
+  location                      = azurerm_resource_group.app_rg.location
+  resource_group_name           = azurerm_resource_group.app_rg.name
+  service_plan_id               = azurerm_service_plan.plan.id
+  https_only                    = true
+  public_network_access_enabled = true
+
+  site_config {
+    minimum_tls_version         = "1.2"
+    ftps_state                  = "Disabled"
+    always_on                   = true
+    http2_enabled               = true
+    scm_use_main_ip_restriction = false
+  }
 }
 
-resource "random_string" "suffix" {
-  length  = 6
-  lower   = true
-  upper   = false
-  numeric = true
-  special = false
-}
-
-output "storage_account_name" {
-  value = azurerm_storage_account.sa.name
+output "app_url" {
+  value = "https://${azurerm_windows_web_app.app.default_hostname}"
 }
